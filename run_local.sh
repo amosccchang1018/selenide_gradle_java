715b72adcfd49006d24691ec49795f515f722dd4
Chi Chun Chang <amosccchang@gmail.com>
2023-09-05 19:27:32 +0800

    feature: update shell scripts

--- run_local.sh
+++ run_local.sh
@@ -8,17 +8,34 @@ else
   SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 fi
 
-PROJECT_DIR="$SCRIPT_DIR/AutomationTrello"
-
+PROJECT_DIR="$SCRIPT_DIR"
 cd "$PROJECT_DIR"
 
-TEST_CASE=$1
-TEST_CASE_FILE="com/testcases/$TEST_CASE.java"
+TEST_CLASS=$1
+TEST_METHOD=$2
+REPORT="${3:-false}"
+SERVE="${4:-false}"
 
-#mvn clean test -Dtest=com.testcases.CucumberRunnerTest
-if [ -f "src/test/java/$TEST_CASE_FILE" ]; then
-  mvn clean test -Dtest=com.testcases.$TEST_CASE
+# If no test method is specified, just use the class name
+if [ -z "$TEST_METHOD" ]; then
+    FULL_TEST_NAME="com.scripts.$TEST_CLASS"
 else
-  echo "Invalid test case name or file does not exist: $TEST_CASE"
+    FULL_TEST_NAME="com.scripts.$TEST_CLASS.$TEST_METHOD"
 fi
 
+# Run the test or the whole test class if no specific method is specified
+if ./gradlew test --tests "$FULL_TEST_NAME" ; then
+
+  # Generate Allure Report if report is set to true
+  if [ "$REPORT" == "true" ]; then
+    ./gradlew allureReport
+  fi
+
+  # Serve Allure Report if serve is set to true
+  if [ "$SERVE" == "true" ]; then
+    ./gradlew allureServe
+  fi
+
+else
+  echo "Failed to execute the test or test class: $FULL_TEST_NAME"
+fi
