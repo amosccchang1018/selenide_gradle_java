023c619f0376acc3260fd3b9604b65c4df403cfd
Chi Chun Chang <amosccchang@gmail.com>
2023-09-06 11:20:32 +0800

    feature: add negative test case

--- src/test/java/com/common/constants/ElementConstants.java
+++ src/test/java/com/common/constants/ElementConstants.java
@@ -14,7 +14,7 @@ public class ElementConstants {
   public static String SEARCH_BAR = "input[placeholder=\"Find what you're looking for\"]";
 
   public static String SELECTED_RING =  ".NAArm";
-  public static String ADD_FAVORITE = "button.styled__ButtonWrapper-sc-1waring-0:nth-child(2) > i:nth-child(1) > svg:nth-child(1)";
+  public static String ADD_FAVORITE = "html body div#__next main.styled__Main-sc-17m6a8v-0.gYNMbX div.styled__Wrapper-sc-c7r2ad-0.gkgaNr.ljs-translate-element div.styled__TopLayout-sc-c7r2ad-1.eKvlth div.styled__ProductDetailsLayout-sc-c7r2ad-3.jlmCUb div.styled__Wrapper-sc-rfz1zv-0.fqAWMn div.styled__AddToCartWrapper-sc-rfz1zv-1.jyzmKL div.styled__Wrapper-sc-ilvwg9-0.hXRkEk div.styled__Buttons-sc-ilvwg9-4.kIQHyB button.styled__ButtonWrapper-sc-1waring-0.jVynKt";
   public static String UNADD_FAVORITE = "button.styled__ButtonWrapper-sc-1waring-0:nth-child(2)";
 
   public static String MY_FAVORITE = "div.styled__Wrapper-sc-1ez4yal-0:nth-child(2) > header:nth-child(1) > div:nth-child(1) > div:nth-child(4) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > i:nth-child(1) > svg:nth-child(1)";
--- src/test/java/com/scripts/WebE2ETest.java
+++ src/test/java/com/scripts/WebE2ETest.java
@@ -10,6 +10,8 @@ import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.DisplayName;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -25,6 +27,8 @@ import static com.codeborne.selenide.Selenide.*;
 public class WebE2ETest extends BaseTest {
 
     @Test
+    @Tag("Positive")
+    @DisplayName("find Product And Add To Cart Test")
     @Description("Find a specific ring and add to my favorite")
     @Severity(SeverityLevel.NORMAL)
     void findProductAndAddToCartTest() {
@@ -51,9 +55,10 @@ public class WebE2ETest extends BaseTest {
 
         // Step 5: Add to my favorite if it's not added
         String pagesource = WebDriverRunner.getWebDriver().getPageSource();
-         if (pagesource.contains(ElementConstants.ADD_FAVORITE)){
-          UIUtil.clickElementByCSS(ElementConstants.ADD_FAVORITE);
-         }
+        if (pagesource.contains(ElementConstants.ADD_FAVORITE)){
+            sleep(3000);
+            UIUtil.clickElementByCSS(ElementConstants.ADD_FAVORITE);
+        }
         else{
              logger.info("already added");
         }
@@ -62,6 +67,43 @@ public class WebE2ETest extends BaseTest {
         UIUtil.clickElementByCSS(ElementConstants.MY_FAVORITE);
         pagesource = WebDriverRunner.getWebDriver().getPageSource();
         assertTrue(pagesource.contains(DESIRE_ITEM));
+
+    }
+
+
+    @Test
+    @Tag("Negative")
+    @DisplayName("find Product And Fail To Add To Favorite Unauthenticated Test")
+    @Description("Find a specific ring but fail to add to my favorite due to being unauthenticated")
+    @Severity(SeverityLevel.NORMAL)
+    void findProductAndFailToAddToFavoriteUnauthenticatedTest() {
+        String DESIRE_ITEM = "Honey Mini Signet";
+        String TestAAccount_AMOS = "User_1";
+
+        // Step 1: Navigate to the homepage
+        open(UrlConstants.MAIN_PAGE);
+
+        // Step 2: Click the search bar
+        SelenideElement buttonElement = $(ElementConstants.SEARCH);
+        buttonElement.shouldBe(Condition.visible, Duration.ofSeconds(5)).click();
+
+        // Step 3: Enter the search keyword and click the item
+        UIUtil.sendKeyByCSS(ElementConstants.SEARCH_BAR, DESIRE_ITEM);
+        UIUtil.clickElementByCSS(ElementConstants.SELECTED_RING);
+
+        // Step 4: Add to my favorite if it's not added
+
+        String pagesource = WebDriverRunner.getWebDriver().getPageSource();
+        if (pagesource.contains(ElementConstants.ADD_FAVORITE)){
+            UIUtil.clickElementByCSS(ElementConstants.ADD_FAVORITE);
+        }
+        else{
+            logger.error("'Add to my favorite' button has issue");
+        }
+
+        // Step 5: See Login block popup
+        pagesource = WebDriverRunner.getWebDriver().getPageSource();
+        assertTrue(pagesource.contains("Please log in"));
 
     }
 
